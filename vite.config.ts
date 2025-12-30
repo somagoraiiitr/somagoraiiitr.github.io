31ffcaaae79db7bd60c10bb60c96a470f8d337b3
Soma Gorai <imsomagorai@gmail.com>
2025-12-31 00:01:33 +0530

    latest build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/somagorai.github.io',
+    base: '/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
